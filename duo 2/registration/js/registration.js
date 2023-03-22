468a1304239574a0c1d4538e9b69b93e024eb50d
jgpettie42 <jgpettie42@tntech.edu>
2023-03-22 15:49:40 -0500

    added more to reg

--- duo 2/registration/js/registration.js
+++ duo 2/registration/js/registration.js
@@ -29,20 +29,177 @@ $('#btnCheckInPreReg').on('click', function(){
 $('#btnCheckInNew').on('click', function(){
     $('#divFirstRegister').slideToggle();
     $('#divCheckIn').slideToggle();
+    $('#divContent').slideToggle();
 })
 
 $('#btnNextToRegistration').on('click', function(){
+    blnError = false;
+    strHTML = '';
+    if($('#txtRegFirstName').val().length < 1){
+        blnError = true;
+        strHTML += '<p>You must enter First Name</p>'
+    }
+    if($('#txtRegLastName').val().length < 1){
+        blnError = true;
+        strHTML += '<p>You must enter Last Name</p>'
+    }
+    if($('#txtRegMiddleInitial').val().length < 1){
+        blnError = true;
+        strHTML += '<p>You must enter Middle Initial</p>'
+    }
+    if($('#selectRegSex').val() == 'Open This Select Menu'){
+        blnError = true;
+        strHTML += '<p>You must select a Sex</p>'
+    }
+    if($('#txtRegDateOfBirth').val().length < 1){
+        blnError = true;
+        strHTML += '<p>You must enter Date of Birth</p>'
+    }
+    if(blnError == true){
+        swal.fire({
+            html: strHTML,
+            icon: 'error',
+        })
+    }
+    else{
+        $('#divFirstRegister').slideToggle();
+        $('#divSecondRegister').slideToggle();
+    } 
+})
+
+$('#btnBackToFirstRegistration').on('click', function(){
     $('#divFirstRegister').slideToggle();
     $('#divSecondRegister').slideToggle();
 })
+
 $('#btnNextToEmergencyContact').on('click', function(){
-    $('#divEmergencyContactRegister').slideToggle();
-    $('#divSecondRegister').slideToggle();
-})
+    blnError = false;
+    if($('#Checkbox').is(':checked')){
+        $('#divEmergencyContactRegister').slideToggle();
+        $('#divSecondRegister').slideToggle();
+    } else{
+        if($('#txtStreetName1').val().length < 1){
+        blnError = true;
+        }
+        if($('#txtCity').val().length < 1){
+            blnError = true;
+        }
+        if($('#txtState').val() == 'Please Select a State'){
+            blnError = true;
+        }
+        if($('#txtCountry').val().length < 1){
+            blnError = true;
+        }
+        if($('#txtZip').val().length < 1){
+            blnError = true;
+        }
+        if(blnError == true){
+            Swal.fire({
+            icon: 'warning',
+            title: 'Are you sure you want to continue without filling out these fields?',
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: `Yes`,
+            denyButtonText: `No`,
+            }).then((result) => {
+            if (result.isConfirmed) {
+                $('#divEmergencyContactRegister').slideToggle();
+                $('#divSecondRegister').slideToggle();
+            } else if (result.isDenied) {
+                
+            }
+            })
+        } else {
+            $('#divEmergencyContactRegister').slideToggle();
+            $('#divSecondRegister').slideToggle();
+        }
+    }
+    
+}) 
 $('#btnNextToLoginInfo').on('click', function(){
-    $('#divEmergencyContactRegister').slideToggle();
-    $('#divLoginInfo').slideToggle();
+    blnError = false;
+    if($('#txtEmergencyFirstName').val().length < 1){
+        blnError = true;
+    }
+    if($('#txtEmergencyLastName').val().length < 1){
+        blnError = true;
+    }
+    if($('#txtEmergencyPhoneNumber').val().length < 1){
+        blnError = true;
+    }
+    if(blnError == true){
+        Swal.fire({
+        icon: 'warning',
+        title: 'Are you sure you would like to proceed without identifying an Emergency Contact?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: `Yes`,
+        denyButtonText: `No`,
+        }).then((result) => {
+        if (result.isConfirmed) {
+            $('#divEmergencyContactRegister').slideToggle();
+            $('#divLoginInfo').slideToggle();
+        } else if (result.isDenied) {
+            
+        }
+        })
+    } else {
+        $('#divEmergencyContactRegister').slideToggle();
+        $('#divLoginInfo').slideToggle();
+    }
+ 
 })
+
+$('#btnBackToEmergencyContact').on('click', function(){
+    $('#divLoginInfo').slideToggle();
+    $('#divEmergencyContactRegister').slideToggle();
+})
+
+$('#btnFinishRegistration').on('click', function(){
+    blnError = false;
+    strHTML = '';
+    if(isValidEmail() == false){
+        blnError = true;
+        strHTML += '<p>The entered email must be a valid email address</p>'
+    }
+    if(isValidPassword() == false){
+        blnError = true;
+        strHTML += '<p>Your password must be at least 8 characters long and contain and uppercase, lowercase, and special character.</p>'
+    }
+    if(blnError == true){
+        swal.fire({
+                html: strHTML,
+                icon: 'error',
+            })
+    } else {
+        swal.fire({
+            icon: 'success',
+            html: '<p>User Successfully Registered</p>',
+            confirmButtonText: 'OK' 
+        }).then((result) => {
+        if (result.isConfirmed) {
+            $('#divFrontPage').slideToggle();
+            $('#divLoginInfo').slideToggle();
+        }
+        })
+        var inputElements = document.getElementsByTagName('input');
+        for (var i=0; i < inputElements.length; i++) {
+            if (inputElements[i].type == 'text') {
+                inputElements[i].value = '';
+            }
+            if (inputElements[i].type == 'date'){
+                inputElements[i].value = '';
+            }
+            if (inputElements[i].type == 'select'){
+                inputElements[i].value = '';
+            }
+            
+        }
+        //also need to slide toggle to dashboard
+    }
+
+})
+
 
 
 $('.btnCheck').on('click',function(){
